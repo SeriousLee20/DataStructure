6f40f933c7803db05c0b480697d5dc5ee3265bb5
SeriousLee20 <LHuiXin01@gmail.com>
2021-01-31 23:32:44 +0800

    Initial commit

---
+++ src/Introduction.java
@@ -0,0 +1,24 @@
+
+/*
+ * Author: Hui Xin
+ * 30 Jan 2021
+ */
+
+public class Introduction {
+    public static void main(String[] args) {
+        /*
+        * List -- dynamic array
+        * Queue -- Linked list/array/stack based
+        * Map -- tree map/hash map/hash table
+        * vehicle -- golf cart/bicycle/smart car
+        * */
+
+        //Static & Dynamic Array
+        /*Static array
+        * == fixed length container
+        * == indexable*/
+
+
+
+    }
+}
