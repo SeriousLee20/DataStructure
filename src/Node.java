97a3343f3b1e780f188a6908136f82928420d2cb
SeriousLee20 <LHuiXin01@gmail.com>
2021-02-05 03:21:19 +0800

    Linked List

--- src/Node.java
+++ src/Node.java
@@ -31,4 +31,30 @@ class LinkedList {
 
         current.next = new Node(data);
     }
+
+    // append at the front
+    public void prepend(int data) {
+        Node newHead = new Node(data);
+        newHead.next = head;
+        head = newHead;
+    }
+
+    public void deleteWithValue(int data) {
+        if(head == null) return;
+        if (head.data == data) {
+            head = head.next;
+            return;
+        }
+
+
+        Node current = head;
+        while (current.next != null) {
+            if (current.next.data == data) {
+                current.next = current.next.next;
+                return;
+            }
+
+            current = current.next;
+        }
+    }
 }
